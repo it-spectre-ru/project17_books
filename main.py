258f8a257ebadd0c7eb9c40763e267c505767442
s <s>
2022-06-29 19:32:25 +0500

    write error

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
-from urllib import response
+from datetime import datetime
+import json
+import time
 import requests
 from bs4 import BeautifulSoup
+import datetime
+import csv
 
+
+start_time = time.time()
 def get_data():
+  cur_time = datetime.datetime.now().strftime("%d_%m_%Y_%H_%M")
+
+  with open(f"labirint_{cur_time}.csv", "w") as file:
+    writer = csv.writer(file)
+
+    writer.writerow(
+      (
+        "Book name",
+        "Author",
+        "Publishing",
+        "Price discount",
+        "Price old",
+        "% Discount",
+        "Status"
+      )
+    )
+
   headers = {
         "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
         "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36"
@@ -14,9 +37,11 @@ def get_data():
   soup = BeautifulSoup(response.text, "lxml")
 
   pages_count = int(soup.find("div", class_="pagination-numbers").find_all("a")[-1].text)
+
+  books_data = []
   
-  # for page in range(1, pages_count + 1):
-  for page in range(1, 2):
+  for page in range(1, pages_count + 1):
+  # for page in range(1, 2):
     url = f"https://www.labirint.ru/genres/2308/?available=1&paperbooks=1&display=table&page={page}"
 
     response = requests.get(url=url, headers=headers)
@@ -66,21 +91,54 @@ def get_data():
         book_status = "Not status data"
 
         
-      print(book_title)
-      print(book_author)
-      print(book_publishing)
-      print(book_new_price)
-      print(book_old_price)
-      print(book_sale)
-      print(book_status)
+      # print(book_title)
+      # print(book_author)
+      # print(book_publishing)
+      # print(book_new_price)
+      # print(book_old_price)
+      # print(book_sale)
+      # print(book_status)
+      # print("#" * 15)
 
-      print("#" * 15)
+      books_data.append(
+        {
+          "book_title": book_title,
+          "book_author": book_author,
+          "book_publishing": book_publishing,
+          "book_new_price": book_new_price,
+          "book_old_price": book_old_price,
+          "book_sale": book_sale,
+          "book_status": book_status,
+        }
+      )
 
+      with open(f"labirint_{cur_time}.csv", "a") as file:
+        writer = csv.writer(file)
+
+      writer.writerow(
+        (
+          book_title,
+          book_author,
+          book_publishing,
+          book_new_price,
+          book_old_price,
+          book_sale,
+          book_status
+        )
+      )
+    
+    print(f"Done {page} / {pages_count}")
+    time.sleep(1)
+  
+  with open(f"labirint_{cur_time}.json", "w") as file:
+    json.dump(books_data, file, indent=4, ensure_ascii=False)
 
 
 
 def main():
   get_data()
+  finish_time = time.time() - start_time
+  print(f"Time: {finish_time}")
 
 
 if __name__ == '__main__':
